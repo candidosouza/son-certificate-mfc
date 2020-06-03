bab9979bfe40c57ba32d0c31d37517e360648baf
Candido Souza <candidosouzza@gmail.com>
2020-06-03 10:04:51 -0300

    Configuração inicial da view da parte do post

--- maratona/views.py
+++ maratona/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from django.views.generic import TemplateView, ListView, DetailView
 from maratona.forms import EmailForm
@@ -13,6 +13,26 @@ class IndexView(TemplateView):
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
         return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        form = EmailForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+
+            try:
+                pass
+                # continuar a lógica para filtrar o email e redirecionar a url para o certificado
+
+            except:
+                response = redirect('index')
+                response['Location'] += '/?error=true'
+                return response
+
+        if form.errors:
+            for field_error in form.errors:
+                response = redirect('skills.portfolio_register')
+                response['Location'] += '?error=%s' % field_error
+                return response
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
