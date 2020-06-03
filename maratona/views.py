570a5ad1e91e075411642769c92c2175e65058ff
Candido Souza <candidosouzza@gmail.com>
2020-06-03 09:34:23 -0300

    Configurando a view para get

--- maratona/views.py
+++ maratona/views.py
@@ -1,7 +1,22 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views import View
+from django.views.generic import TemplateView, ListView, DetailView
+from maratona.forms import EmailForm
 
 
-class Index(TemplateView):
+class IndexView(TemplateView):
+    model = None
     template_name = 'index.html'
+    form_class = EmailForm
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return self.render_to_response(context)
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        form = self.form_class
+        context['form'] = form
+        return context
+
